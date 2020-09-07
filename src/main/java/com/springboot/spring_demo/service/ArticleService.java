616a12021dcb682baa9d7c7a649bc0c8664b2ff2
Punk_Off <pankov.kirill1997@yandex.ru>
2020-09-07 13:42:40 +0300

    Отображение статей на главной

--- src/main/java/com/springboot/spring_demo/service/ArticleService.java
+++ src/main/java/com/springboot/spring_demo/service/ArticleService.java
@@ -5,6 +5,13 @@ import com.springboot.spring_demo.model.Article;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Comparator;
+import java.util.List;
+import java.util.Spliterator;
+import java.util.Spliterators;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
 @Service
 public class ArticleService {
     @Autowired
@@ -12,6 +19,13 @@ public class ArticleService {
 
     public void save(Article article) {
         repository.save(article);
+    }
+
+    public List<Article> getAll() {
+        return StreamSupport.stream(
+                Spliterators.spliteratorUnknownSize(repository.findAll().iterator(), Spliterator.NONNULL), false)
+                .sorted(Comparator.reverseOrder())
+                .collect(Collectors.toList());
     }
 
 }
