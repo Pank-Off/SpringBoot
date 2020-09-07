c1bffd945047e12ca8f0f92b528b81ecbc764e65
Punk_Off <pankov.kirill1997@yandex.ru>
2020-09-07 11:49:45 +0300

    Создание приложения spring boot

---
+++ src/main/java/com/springboot/spring_demo/ApplicationLauncher.java
@@ -0,0 +1,11 @@
+package com.springboot.spring_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationLauncher {
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationLauncher.class,args);
+    }
+}
